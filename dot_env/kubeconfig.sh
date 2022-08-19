3ba32abf0b23c13221aa2ac95b2867b03b94cebc
Paul Landázuri <5787964+LandazuriPaul@users.noreply.github.com>
2022-08-19 10:41:49 +0100

    feat: Replace scripts with normal templates + fix kubeconfig

--- dot_env/kubeconfig.sh
+++ dot_env/kubeconfig.sh
@@ -2,12 +2,17 @@
 DEFAULT_KUBECONFIG_FILE="$HOME/.kube/config"
 EXTRA_KUBECONFIG_FOLDER="$HOME/.kube/configs"
 
-# Default kubeconfig ~/.kube/config
-[ -f "${DEFAULT_KUBECONFIG_FILE}" ] && export KUBECONFIG="$DEFAULT_KUBECONFIG_FILE"
-
 # Extra kubeconfig files
 [ -d "${EXTRA_KUBECONFIG_FOLDER}" ] && \
     for kubeconfigFile in $(find "${EXTRA_KUBECONFIG_FOLDER}" -type f -name "*.yml" -o -name "*.yaml");
     do
         export KUBECONFIG="$kubeconfigFile:$KUBECONFIG"
     done
+
+# Default kubeconfig ~/.kube/config
+[ -f "${DEFAULT_KUBECONFIG_FILE}" ] && export KUBECONFIG="$DEFAULT_KUBECONFIG_FILE:$KUBECONFIG"
+
+# If no default, make sure the KUBECONFIG doesn't end with ":"
+if [[ "${KUBECONFIG: -1}" == ":" ]]; then
+  KUBECONFIG=${KUBECONFIG:0:-1}
+fi
