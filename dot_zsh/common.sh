8ee0040a0523dcf7043820e9a440ddc5ecebd14a
Paul Landázuri <5787964+LandazuriPaul@users.noreply.github.com>
2022-08-20 14:01:33 +0100

    fix: clean env/integrations

--- dot_zsh/common.sh
+++ dot_zsh/common.sh
@@ -1,3 +1,15 @@
+# Rust integration
+[ -f "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
+
+# Pyenv
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+
+# nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # kubectl completion
 source <(kubectl completion zsh)
 
