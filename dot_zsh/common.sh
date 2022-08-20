0f118fd88cb65d599b89921ecb58cb35f8f0bb09
Paul Landázuri <5787964+LandazuriPaul@users.noreply.github.com>
2022-08-20 14:13:24 +0100

    fix: jenv path to .env

--- dot_zsh/common.sh
+++ dot_zsh/common.sh
@@ -6,6 +6,12 @@ if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
 
+# jEnv integration
+if [ -x "$(command -v jenv)" ]; then
+    export PATH="$HOME/.jenv/bin:$PATH"
+    eval "$(jenv init -)"
+fi
+
 # nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
